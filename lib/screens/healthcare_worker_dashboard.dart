4772b95578c69a225708df4203be6e49a8c691c7
Aarya-k05 <katikaraarya@gmail.com>
2025-09-09 06:49:11 +0000

    9

--- lib/screens/healthcare_worker_dashboard.dart
+++ lib/screens/healthcare_worker_dashboard.dart
@@ -60,16 +60,4 @@ class _DashboardCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTap,
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-        elevation: 4,
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(icon, size: 48, color: Theme.of(context).primaryColor),
-              const SizedBox(height: 12),
-              Text(
-                title,
-                style: const TextStyle(
-                  fontSize:
+      child: C
